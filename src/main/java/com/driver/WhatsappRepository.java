7c4df65a1c3dc80a60a6ebe6f2a090fec35e7e1e
Manikandan <manikandan400800@gmail.com>
2023-07-22 14:55:42 +0530

    Assignment 3rd try

--- src/main/java/com/driver/WhatsappRepository.java
+++ src/main/java/com/driver/WhatsappRepository.java
@@ -56,9 +56,13 @@ public class WhatsappRepository{
     }
 
     public int sendMessage(Message message, User sender, Group group) throws Exception {
+
         if(!groupListMap.containsKey(group))
             throw new Exception("Group does not exist");
-        if(!groupListMap.get(group).contains(sender))
+
+        List<User> users = groupListMap.get(group);
+
+        if(!ifExistInGroup(sender, users))
             throw new Exception("You are not allowed to send message");
 
         List<Message> messages = groupMessages.getOrDefault(group, new ArrayList<>());
@@ -73,10 +77,7 @@ public class WhatsappRepository{
     }
 
     public String changeAdmin(User approver, User user, Group group) throws Exception {
-        //Throw "Group does not exist" if the mentioned group does not exist
-        //Throw "Approver does not have rights" if the approver is not the current admin of the group
-        //Throw "User is not a participant" if the user is not a part of the group
-        //Change the admin of the group to "user" and return "SUCCESS". Note that at one time there is only one admin and the admin rights are transferred from approver to user.
+
         if(!groupListMap.containsKey(group))
             throw new Exception("Group does not exist");
 
@@ -93,13 +94,6 @@ public class WhatsappRepository{
     }
 
     public int removeUser(User user) throws Exception {
-        //A user belongs to exactly one group
-        //If user is not found in any group, throw "User not found" exception
-        //If user is found in a group and it is the admin, throw "Cannot remove admin" exception
-        //If user is not the admin, remove the user from the group, remove all its messages from all the databases, and update relevant attributes accordingly.
-        //If user is removed successfully, return (the updated number of users in the group +
-        // the updated number of messages in group +
-        // the updated number of overall messages)
 
         Group group = null;
         for(Group group1 : groupListMap.keySet()){
